b1b9a69c8ad66a92b51cb20be024d0b1d212bc98
Cristohper Alejandro Ponce Rivera <cristoferponcerivera@gmail.com>
2026-01-19 09:59:24 -0600

    Errores arreglados, joystick y motores invertidos

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,97 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import static frc.robot.Constants.OperatorConstants.*;
+import static frc.robot.Constants.FuelConstants.*;
+import frc.robot.commands.Autos;
+import frc.robot.subsystems.CANDriveSubsystem;
+import frc.robot.subsystems.CANFuelSubsystem;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems
+  private final CANDriveSubsystem driveSubsystem = new CANDriveSubsystem();
+  private final CANFuelSubsystem ballSubsystem = new CANFuelSubsystem();
+
+  // The driver's controller
+  private final edu.wpi.first.wpilibj.Joystick driverJoystick = new edu.wpi.first.wpilibj.Joystick(
+      DRIVER_CONTROLLER_PORT);
+
+  // The autonomous chooser
+  private final SendableChooser<Command> autoChooser = new SendableChooser<>();
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    configureBindings();
+
+    // Set the options to show up in the Dashboard for selecting auto modes. If you
+    // add additional auto modes you can add additional lines here with
+    // autoChooser.addOption
+    autoChooser.setDefaultOption("Autonomous", Autos.exampleAuto(driveSubsystem, ballSubsystem));
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the {@link Trigger#Trigger(java.util.function.BooleanSupplier)}
+   * constructor with an arbitrary predicate, or via the named factories in
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses
+   * for {@link CommandXboxController Xbox}/
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick 
+   */
+  private void configureBindings() {
+
+    // Button 2 (Thumb): Intake Fuel
+    new Trigger(() -> driverJoystick.getRawButton(2))
+        .whileTrue(ballSubsystem.runEnd(() -> ballSubsystem.intake(), () -> ballSubsystem.stop()));
+
+    // Trigger (Button 1): Spin up and launch
+    // Uses spinUpCommand + launchCommand combination
+    new Trigger(driverJoystick::getTrigger)
+        .whileTrue(ballSubsystem.spinUpCommand().withTimeout(SPIN_UP_SECONDS)
+            .andThen(ballSubsystem.launchCommand())
+            .finallyDo(() -> ballSubsystem.stop()));
+
+    // Button 3 (Top): Eject fuel
+    new Trigger(() -> driverJoystick.getRawButton(3))
+        .whileTrue(ballSubsystem.runEnd(() -> ballSubsystem.eject(), () -> ballSubsystem.stop()));
+
+    // Set the default command for the drive subsystem to the command provided by
+    // factory with the values provided by the joystick axes on the driver
+    // controller. The Y axis of the controller is inverted so that pushing the
+    // stick away from you (a negative value) drives the robot forwards (a positive
+    // value). The Z-axis (twist/rotation) is also inverted so a positive value 
+    // (stick rotated clockwise) results in clockwise rotation (front of the robot 
+    // turning right). Both axes are also scaled so movement is more controllable.
+    driveSubsystem.setDefaultCommand(
+        driveSubsystem.driveArcade(
+            () -> -driverJoystick.getY() * DRIVE_SCALING,
+            () -> -driverJoystick.getZ() * ROTATION_SCALING));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return autoChooser.getSelected();
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.function.DoubleSupplier;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class CANDriveSubsystem extends SubsystemBase {
+  private static final int kCanID_neo1 = 1;
+  private static final int kCanID_neo2 = 2;
+  private static final int kCanID_neo3 = 3;
+  private static final int kCanID_neo4 = 4;
+
+  private static final MotorType kMotorType_neo = MotorType.kBrushless;
+
+  private final SparkMax m_motor_neo1 = new SparkMax(kCanID_neo1, kMotorType_neo);
+  private final SparkMax m_motor_neo2 = new SparkMax(kCanID_neo2, kMotorType_neo);
+  private final SparkMax m_motor_neo3 = new SparkMax(kCanID_neo3, kMotorType_neo);
+  private final SparkMax m_motor_neo4 = new SparkMax(kCanID_neo4, kMotorType_neo);
+
+  private final DifferentialDrive m_drive = new DifferentialDrive(
+    (double speed) -> {
+      m_motor_neo1.set(speed);
+      m_motor_neo2.set(speed);
+    },
+    (double speed) -> {
+      m_motor_neo3.set(speed);
+      m_motor_neo4.set(speed);
+    }
+  );
+
+  public CANDriveSubsystem() {
+     m_motor_neo3.setInverted(true);
+     m_motor_neo4.setInverted(true);
+  }
+
+  /** Manejo arcade (fwd = adelante/atrás, rot = giro). */
+  public void arcadeDrive(double fwd, double rot) {
+    m_drive.arcadeDrive(fwd, rot);
+  }
+
+  /** Devuelve un Command que maneja arcade usando ejes del joystick en vivo. */
+  public Command driveArcade(DoubleSupplier fwd, DoubleSupplier rot) {
+    return Commands.run(() -> arcadeDrive(fwd.getAsDouble(), rot.getAsDouble()), this);
+  }
+}

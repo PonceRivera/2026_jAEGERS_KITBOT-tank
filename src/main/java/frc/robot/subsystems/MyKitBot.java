c1be63ced3ed776db57dfffaef9876b62b4839af
Cristohper Alejandro Ponce Rivera <cristoferponcerivera@gmail.com>
2026-01-29 23:32:25 -0600

    Update robot code and MyKitBot

--- src/main/java/frc/robot/subsystems/MyKitBot.java
+++ src/main/java/frc/robot/subsystems/MyKitBot.java
@@ -19,19 +19,15 @@ public class MyKitBot extends SubsystemBase {
     public MyKitBot() {
         m_Take = new SparkMax(6, kMotorType);
         m_Disparo = new SparkMax(5, kMotorType);
-    //SparkMaxConfig disparoConfig = new SparkMaxConfig();
-     //SparkMaxConfig takeConfig = new SparkMaxConfig();
-
+    
         takeConfig.inverted(false);
         disparoConfig.inverted(true);
 
-        // Do not persist these parameters to the device to avoid permanent
-        // changes and cross-command interference.
+        
     m_Take.configure(takeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     m_Disparo.configure(disparoConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     }
-    /*
-     * ---------- NOTAS DE INVERSIÓN / DÓNDE CAMBIAR ----------
+    /*           
      *
      * 1) Inversión por comando (recomendado):
      *    - Edita SOLO el valor pasado a `set(...)` dentro del comando
@@ -55,38 +51,20 @@ public class MyKitBot extends SubsystemBase {
    public Command Disparo() {
         return runEnd(() -> {
             m_Disparo.set(1);
-            m_Take.set(0.5);
+            m_Take.set(0.7);
         }, () -> {
             m_Disparo.set(0);
             m_Take.set(0);
         });
    }
-//     public Command Disparo2() {
-//     return runEnd(() -> {
-//         m_Disparo.set(.9);
-//     }, () -> {
-//         m_Disparo.set(0);
-//     });
-// }
+
  public Command DisparoOFF() {
     return runEnd(() -> {
         m_Disparo.set(0);
     }, () -> {       
     });
 }
-/*
-public Command Take() {
-    return runOnce(() -> {
-        m_Take.set(0.6);
-        m_Disparo.set(0.6);
-    });
-}
-*/
 
-//public Command Take() {
-//     return runEnd(() -> m_Take.set(0.6), () -> m_Take.set(0));
-//     });
-// }
 public Command TakeOFF() {
     return runOnce(() -> {
         m_Take.set(0.6);  
@@ -96,8 +74,8 @@ public Command TakeOFF() {
 public Command Take2() {
 
     return runEnd(() -> {
-        m_Take.set(-0.8);
-        m_Disparo.set(.55);
+        m_Take.set(-1);
+        m_Disparo.set(.53);
     }, () -> {
         m_Take.set(0);
         m_Disparo.set(0);
@@ -105,100 +83,7 @@ public Command Take2() {
 }
  public Command TakeOUT() {
     return runOnce(() -> {
-        m_Take.set(1); 
+        m_Take.set(7); 
         m_Disparo.set(-0.6);
     });
 }}
-
-
-/*package frc.robot.subsystems;
-
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-
-public class MyKitBot extends SubsystemBase {
-    private SparkMax m_Disparo;
-    private SparkMax m_Take;
-    private static final  MotorType kMotorType = MotorType.kBrushed;
-
-    public MyKitBot() {
-        m_Take = new SparkMax(6, kMotorType);
-        m_Disparo = new SparkMax(5, kMotorType);
-        // Default configs: set inversion via config and apply using non-deprecated configure(SparkMaxConfig)
-        SparkMaxConfig takeConfig = new SparkMaxConfig();
-        takeConfig.inverted(false);
-        SparkMaxConfig disparoConfig = new SparkMaxConfig();
-        disparoConfig.inverted(true);
-
-
-    m_Disparo.configure(disparoConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_Take.configure(takeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    }
-    
-
-   public Command Disparo() {
-        return runOnce(() -> {
-            SparkMaxConfig takeConfig = new SparkMaxConfig();
-            takeConfig.inverted(false);
-            m_Take.configure(takeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
-            m_Disparo.set(1);
-            m_Take.set(0.5);
-        });
-   }
-    public Command Disparo2() {
-    return runOnce(() -> {
-        m_Disparo.set(.9);
-        
-    });
-}
- public Command DisparoOFF() {
-    return runOnce(() -> {
-        m_Disparo.set(0);
-        
-    });
-}
-public Command Take() {
-    return runOnce(() -> {
-        m_Take.set(0.6);
-        m_Disparo.set(0.6);
-    });
-}
-
-//public Command Take() {
-//     return runEnd(() -> m_Take.set(0.6), () -> m_Take.set(0));
-//     });
-// }
-public Command TakeOFF() {
-    // m_Take non-inverted, m_Disparo inverted for this command only.
-    return runEnd(() -> {
-        m_Take.set(0.6);   // non-inverted output (same direction as default)
-        m_Disparo.set(-0.6); // inverted output just for this command
-    }, () -> {
-        m_Take.set(0);
-        m_Disparo.set(0);
-    });
-public Command Take2() {
-   /*
-    * funcionando
-    */ 
-    /*SparkMaxConfig takeConfig = new SparkMaxConfig();
-    takeConfig.inverted(true);
-    m_Take.configure(takeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
-    SparkMaxConfig disparoConfig = new SparkMaxConfig();
-    disparoConfig.inverted(true);
-    m_Disparo.configure(disparoConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
-    return runOnce(() -> {
-        m_Take.set(1);
-        m_Disparo.set(0.55);
-    });
-}
-} */
